f623196653b01a57a0ae6e0e66f29dfc5c2d4dbe
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-04-29 17:10:39 -0300

    BCrypt password

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,27 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODE = " + passwordEncoder.encode("123456"));
+		
+		boolean result = passwordEncoder.matches("123456", "$2a$10$zf0vwgqqBmzGaJwC0JwT9OYxuQegHbglMT1o5X8J0/I.2D/KXzhWi");
+		System.out.println("RESULTADO = " + result);
 	}
 
 }
--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$zf0vwgqqBmzGaJwC0JwT9OYxuQegHbglMT1o5X8J0/I.2D/KXzhWi');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$zf0vwgqqBmzGaJwC0JwT9OYxuQegHbglMT1o5X8J0/I.2D/KXzhWi');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
